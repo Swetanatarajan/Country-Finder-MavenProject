8cc34c6712a7609cada592ed96487995f40d9e3e
Sweta Natarajan <68581344+Swetanatarajan@users.noreply.github.com>
2020-07-23 15:29:08 +0530

    Add files via upload

---
+++ Maven_Project/Country_Finder_1/src/main/java/CFind/CountryFinder.java
@@ -0,0 +1,24 @@
+package CFind;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import java.io.IOException;
+import java.util.Scanner;
+public class CountryFinder {
+        public String findCountry(String countryCode) throws IOException {
+
+            String url = "https://restcountries.eu/rest/v2/alpha/" + countryCode;
+            CloseableHttpClient httpclient = HttpClients.createDefault();
+            HttpGet httpget = new HttpGet(url);
+            HttpResponse httpresponse = httpclient.execute(httpget);
+            Scanner sc = new Scanner(httpresponse.getEntity().getContent());
+            System.out.println(httpresponse.getStatusLine());
+            String result = "";
+            while (sc.hasNext()) {
+                result = result + sc.nextLine();
+            }
+            return result;
+        }
+    }
+
